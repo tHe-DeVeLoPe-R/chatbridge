0c5243b941a4ea1f3d89215fc76f948ff7d306e5
saad zafar <codewithsaadzafar@gmail.com>
2024-08-30 21:07:15 +0500

    Footer jsx file

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <div>
+      <p className='footer-para1'>Developed by Saad Zafar | codewithsaadzafar@gmail.com</p>
+      <small>Connect with me on <a href="https://www.linkedin.com/in/saad-zafar-12777621a/">Linkedin</a> for guide</small>
+    </div>
+  )
+}
