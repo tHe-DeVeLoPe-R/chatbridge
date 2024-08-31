813fc2d3cba2f1caecfebca9d7488b34b0320138
saad zafar <codewithsaadzafar@gmail.com>
2024-08-31 08:59:33 +0500

    intro update, remove 1 line from intro

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -8,7 +8,7 @@ export default function Intro() {
       <h1 className='heading'>ChatBridge - What this  product is?</h1>
       <img className='logo' src={logoimage} alt="" />
       <p className='intro-para'>
-        Introducing our innovative messaging solution &nbsp;<b>Chat Bridge</b> that transforms how you connect with contacts on WhatsApp. With our product, you can send messages to unsaved numbers effortlessly, eliminating the need to save contacts for quick, one-time communications. Whether you need to reach out to a local print shop, a service provider, or anyone else for a brief exchange, our tool streamlines the process, saving you time and effort while keeping your contact list uncluttered. Embrace convenience and efficiency with direct messaging capabilities that simplify your communication needs.
+        Introducing our innovative whatsapp messaging solution &nbsp;<b>Chat Bridge</b>. With our product, you can send messages to unsaved numbers effortlessly, eliminating the need to save contacts for quick, one-time communications. Whether you need to reach out to a local print shop, a service provider, or anyone else for a brief exchange, our tool streamlines the process, saving you time and effort while keeping your contact list uncluttered. Embrace convenience and efficiency with direct messaging capabilities that simplify your communication needs.
 
       </p>
 
